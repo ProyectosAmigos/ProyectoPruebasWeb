1cf5d1366652388d84b7115a43ea03412d1fc611
DavidGarciaRobles <david.garcia.robles@gmail.com>
2015-04-26 19:41:10 +0200

    Nueva clase para valores y metodos generales

---
+++ webPruebasAmigos/Clases/Conexion.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace webPruebasAmigos.Clases
+{
+    public class Conexion
+    {
+        public void Open()
+        {
+
+        }
+    }
+
+}
